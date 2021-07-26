75920b8720035ea6e5a94e53ee450ea941bf7da6
Blake Yeboah <blakekyeboah@gmail.com>
2021-07-26 17:21:17 +1000

    Created validateRegisterInput Middleware

---
+++ server/middleware/validation.ts
@@ -0,0 +1,37 @@
+// Types
+import { Request, Response, NextFunction } from 'express'
+
+const validateEmail = (email: string) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export const validateRegisterInput = async (req: Request, res: Response, next: NextFunction) => {
+    const { name, username, password, email, bio } = req.body;
+
+    if (!name) {
+        return res.status(400).json({ msg: "Please enter a name."})
+    }
+
+    if (!username) {
+        return res.status(400).json({ msg: "Please enter a username."})
+    }
+
+    if (!password) {
+        return res.status(400).json({ msg: "Please enter a password."})
+    } else if (password.length < 8) {
+        return res.status(400).json({ msg: "Password must be 8 characters or longer."});
+    }
+
+    if (!email) {
+        return res.status(400).json({ msg: "Please enter an email."})
+    } else if (!validateEmail(email)) {
+        return res.status(400).json({ msg: "Email is invalid"});
+    }
+
+    if (!bio) {
+        return res.status(400).json({ msg: "Please enter a bio."})
+    }
+
+    next();
+}
