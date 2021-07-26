a35b124fb8444ce8bd97d97dd8adb9f53a75e6f6
Blake Yeboah <blakekyeboah@gmail.com>
2021-07-26 17:23:03 +1000

    Updated Validation

--- server/middleware/validation.ts
+++ server/middleware/validation.ts
@@ -10,27 +10,29 @@ export const validateRegisterInput = async (req: Request, res: Response, next: N
     const { name, username, password, email, bio } = req.body;
 
     if (!name) {
-        return res.status(400).json({ msg: "Please enter a name."})
+        return res.status(400).json({ msg: "Please enter a name."});
+    } else if (name.length > 20) {
+        return res.status(400).json({ msg: "Your name can only be 20 characters long."});
     }
 
     if (!username) {
-        return res.status(400).json({ msg: "Please enter a username."})
+        return res.status(400).json({ msg: "Please enter a username."});
     }
 
     if (!password) {
-        return res.status(400).json({ msg: "Please enter a password."})
+        return res.status(400).json({ msg: "Please enter a password."});
     } else if (password.length < 8) {
         return res.status(400).json({ msg: "Password must be 8 characters or longer."});
     }
 
     if (!email) {
-        return res.status(400).json({ msg: "Please enter an email."})
+        return res.status(400).json({ msg: "Please enter an email."});
     } else if (!validateEmail(email)) {
         return res.status(400).json({ msg: "Email is invalid"});
     }
 
     if (!bio) {
-        return res.status(400).json({ msg: "Please enter a bio."})
+        return res.status(400).json({ msg: "Please enter a bio."});
     }
 
     next();
