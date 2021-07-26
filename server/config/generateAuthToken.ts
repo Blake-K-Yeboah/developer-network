38a00398cbac7c2424f4f944d5362e58b3fd33b7
Blake Yeboah <blakekyeboah@gmail.com>
2021-07-26 17:11:42 +1000

    Created Generate Auth Token Config File

---
+++ server/config/generateAuthToken.ts
@@ -0,0 +1,24 @@
+// JWT package
+import jwt from "jsonwebtoken";
+
+// Interfaces
+import { IUser } from "../config/interface";
+
+export default (user: IUser) => {
+    const secret = process.env.JWT_SECRET;
+
+    return jwt.sign(
+        {
+            _id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email,
+            profilePicture: user.profilePicture,
+            bio: user.bio,
+            friends: user.friends,
+            createdAt: user.createdAt
+        },
+        secret!,
+        { expiresIn: "1h" }
+    );
+}
