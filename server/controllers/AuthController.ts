51378cbc7ad8ac1695c2101e0f9c99c5c7135d74
Blake Yeboah <blakekyeboah@gmail.com>
2021-08-01 21:58:38 +1000

    Added Register User Functionality

--- server/controllers/AuthController.ts
+++ server/controllers/AuthController.ts
@@ -1,9 +1,50 @@
+//  Express Types
 import { Request, Response } from "express";
 
+// Modules
+import bcrypt from "bcryptjs";
+
+// User Model
+import { User } from "../models/user";
+
+// Generate Token Function
+import generateAuthToken from "../config/generateAuthToken";
+
 // Register User
-const registerUser = (req: Request, res: Response): void => {
-    // TODO: Register User
-    res.send("Register");
+const registerUser = async (req: Request, res: Response): Promise<Response> => {
+    
+    // Check if email already in use
+    const userByEmail = await User.findOne({ email: req.body.email });
+
+    if (userByEmail) {
+        return res.status(400).json({ msg: "Email already used." });
+    }
+
+    // Check if username already in use
+    const userByUsername = await User.findOne({ username: req.body.username });
+
+    if (userByUsername) {
+        return res.status(400).json({ msg: "Username already used." });
+    }
+
+    // Hash password for safe storage
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+    // Create and save new user
+    const newUser = new User({
+        name: req.body.name,
+        username: req.body.username,
+        password: hashedPassword,
+        email: req.body.email,
+        bio: req.body.bio
+    });
+
+    const savedUser = newUser.save();
+
+    // Generate and respond with JWT
+    const token = generateAuthToken(savedUser);
+
+    return res.json({ token });
 }
 
 const AuthController = {
