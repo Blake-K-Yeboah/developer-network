aabaa2bb81129c77c756dce02dc8689b7b4cd34c
Blake Yeboah <blakekyeboah@gmail.com>
2021-08-07 21:11:03 +1000

    Created /reset-password route with auth middleware

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 // Middleware
 import { validateRegisterInput, validateLoginInput, validateResetPasswordFromCodeInput } from "../middleware/validation";
+import authenticate from "../middleware/authenticate";
 
 // Auth Controller
 import AuthController from "../controllers/AuthController";
@@ -20,6 +21,9 @@ router.post("/request-password-reset", AuthController.requestPasswordReset);
 
 // Reset Password From Code
 router.put("/reset-password-from-code", validateResetPasswordFromCodeInput, AuthController.resetPasswordFromCode);
+
+// Reset Password While Logged In
+router.put("/reset-password", authenticate, AuthController.resetPassword)
 
 // Export Router
 export default router;
