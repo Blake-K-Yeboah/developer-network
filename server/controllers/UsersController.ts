61681c5f62e84cf919ea4086dad6559c7a118d01
Blake Yeboah <blakekyeboah@gmail.com>
2021-08-16 20:14:34 +1000

    Created UsersController with getAllUsers function

---
+++ server/controllers/UsersController.ts
@@ -0,0 +1,25 @@
+//  Express Types
+import { Request, Response } from "express";
+
+// Interfaces
+import { AuthRequest, IUser } from "../config/interface";
+
+// User Model
+import { User } from "../models/user";
+
+// Get all users
+const getAllUsers = async (req: AuthRequest, res: Response): Promise<Response> => {
+
+    // Fetch all users from db
+    const users: IUser[] = await User.find({});
+
+    // Return all users
+    return res.json({
+        count: users.length,
+        results: users
+    });
+}
+
+export default {
+    getAllUsers
+}
