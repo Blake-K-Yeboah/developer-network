d94ba0443f08cced0ef6dc56655659f7fa9402d5
Blake Yeboah <blakekyeboah@gmail.com>
2021-09-21 20:18:47 +1000

    Created Client React App

---
+++ client/README.md
@@ -0,0 +1,5 @@
+# Client Of Developer Network
+
+The front end of this social network.
+
+> This project is still being built.
---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+// Stylesheet
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      Hello There!
+    </div>
+  );
+}
+
+export default App;
