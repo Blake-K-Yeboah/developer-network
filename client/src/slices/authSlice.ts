5b44eba4146179d42981cc14d46162dc63c595ce
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-02 17:43:28 +1000

    Added initialState Type

--- client/src/slices/authSlice.ts
+++ client/src/slices/authSlice.ts
@@ -1,24 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 import jwt_decode from "jwt-decode";
 
+// Types
+import { IAuthSliceState } from "../types";
+
+const initialState: IAuthSliceState = {
+    user: null,
+    isAuthenticated: false,
+    error: null,
+    token: null,
+};
+
 export const authSlice: any = createSlice({
     name: "auth",
-    initialState: {
-      user: null,
-      isAuthenticated: false,
-      errors: null,
-      token: null,
-    },
+    initialState,
     reducers: {
         setUser: (state, action) => {
             state.user = action.payload ? jwt_decode(action.payload) : null;
             state.isAuthenticated = action.payload ? true : false;
             state.token = action.payload || null;
-         },
-         setErrors: (state, action) => {
-            state.errors = action.payload;
-         },
-    }
+        },
+        setError: (state, action) => {
+            state.error = action.payload;
+        },
+    },
 });
 
 export const { setUser, setErrors } = authSlice.actions;
