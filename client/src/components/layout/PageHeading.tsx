39aed145c25eaf3ebfcb9241ac0028c60f780256
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-02 17:44:32 +1000

    Created PageHeading Component

---
+++ client/src/components/layout/PageHeading.tsx
@@ -0,0 +1,7 @@
+import { StyledPageHeading } from "../styles/PageHeading.styled";
+
+const PageHeading = () => {
+    return <StyledPageHeading>Dev Network</StyledPageHeading>;
+};
+
+export default PageHeading;
