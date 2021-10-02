b0bdceea7e0fe8aa6e1cff23a0aabd71c50ff9dc
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-02 17:10:00 +1000

    Added Provider Around App Component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// React Stuff
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+
+// App Component
+import App from "./App";
+
+// Redux
+import store from "./store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
