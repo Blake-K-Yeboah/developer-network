41428314e0143f9ffc3d7700469df00dfecefdcd
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-03 17:53:14 +1000

    Created Theme with primary colors

---
+++ client/src/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+    colors: {
+        lightGray: "#7E7E7E",
+        primaryBlue: "#D2E1FF",
+        primaryRed: "#FFD2D2",
+        fadedWhite: "rgba(255, 255, 255, 0.2)",
+    },
+};
