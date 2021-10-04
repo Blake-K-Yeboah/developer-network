318a5d2f93372af5510764bb89f58aa9632460bb
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-04 19:07:13 +1000

    Addded Switching Functionality Between Sign In and Sign Up

--- client/src/components/pages/Home/FormContainer.tsx
+++ client/src/components/pages/Home/FormContainer.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Styled Components
 import {
+    StyledButton,
     StyledFormContainer,
     StyledFormSubText,
     StyledFormTitle,
@@ -10,6 +11,7 @@ import {
 
 // Regular Components
 import SignInForm from "./SignInForm";
+import SignUpForm from "./SignUpForm";
 
 interface IProps {
     action: string;
@@ -17,16 +19,49 @@ interface IProps {
 }
 
 const FormContainer: React.FC<IProps> = (props) => {
-    const rightPanelActive: boolean = props.action === "sign-up";
+    const [rightPanelActive, setRightPanelActive] = useState<boolean>(
+        props.action === "sign-up"
+    );
+
+    const [animate, setAnimate] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setRightPanelActive(!rightPanelActive);
+        setAnimate(true);
+
+        setTimeout(() => setAnimate(false), 750);
+    };
 
     return (
         <StyledFormContainer rightPanelActive={rightPanelActive}>
-            <StyledPanel>
-                <StyledFormTitle>Sign In</StyledFormTitle>
-                <StyledFormSubText>Welcome back developer!</StyledFormSubText>
-                <SignInForm />
+            <StyledPanel animate={animate}>
+                <StyledFormTitle>
+                    {rightPanelActive ? "Sign Up" : "Sign In"}
+                </StyledFormTitle>
+                <StyledFormSubText>
+                    {rightPanelActive
+                        ? "Welcome to Dev Network!"
+                        : "Welcome back developer!"}
+                </StyledFormSubText>
+                {rightPanelActive ? <SignUpForm /> : <SignInForm />}
             </StyledPanel>
-            <div></div>
+            <StyledPanel animate={animate}>
+                <StyledFormTitle>
+                    {rightPanelActive ? "Sign In" : "Sign Up"}
+                </StyledFormTitle>
+                <StyledFormSubText>
+                    {rightPanelActive
+                        ? "Welcome back to Dev Network! Hit the button below to sign in to your account."
+                        : "Dont have an account? Dont worry, sign up to the ultimate social network for developers."}
+                </StyledFormSubText>
+                <StyledButton
+                    colorScheme={rightPanelActive ? "blue" : "red"}
+                    notCenter
+                    onClick={handleClick}
+                >
+                    {rightPanelActive ? "Sign In" : "Sign Up"}
+                </StyledButton>
+            </StyledPanel>
         </StyledFormContainer>
     );
 };
--- client/src/components/pages/Home/SignInForm.tsx
+++ client/src/components/pages/Home/SignInForm.tsx
@@ -1,3 +1,4 @@
+// Styled Components
 import {
     StyledForm,
     StyledInput,
@@ -5,11 +6,28 @@ import {
     StyledButton,
 } from "../../styles/FormContainer.styled";
 
+// Icons
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+// React and useState hook
 import React, { useState } from "react";
+
+// User Input Interface
+interface IUserInput {
+    email: string;
+    password: string;
+}
 
 const SignInForm = () => {
     const [show, setShow] = useState<boolean>(false);
+    const [userInput, setUserInput] = useState<IUserInput>({
+        email: "",
+        password: "",
+    });
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserInput({ ...userInput, [e.target.id]: e.target.value });
+    };
 
     const handleBtnClick = () => {
         setShow(!show);
@@ -21,17 +39,28 @@ const SignInForm = () => {
 
     return (
         <StyledForm onSubmit={handleFormSubmit}>
-            <StyledInput placeholder="Email: " />
+            <StyledInput
+                placeholder="Email: "
+                value={userInput.email}
+                type="email"
+                id="email"
+                onChange={onChange}
+            />
             <StyledInputGroup>
                 <StyledInput
                     placeholder="Password: "
                     type={show ? "text" : "password"}
+                    value={userInput.password}
+                    id="password"
+                    onChange={onChange}
                 />
                 <button onClick={handleBtnClick}>
                     {show ? <AiFillEyeInvisible /> : <AiFillEye />}
                 </button>
             </StyledInputGroup>
-            <StyledButton colorScheme="blue">Sign In</StyledButton>
+            <StyledButton colorScheme="blue" type="submit">
+                Sign In
+            </StyledButton>
         </StyledForm>
     );
 };
