67ccbab700e7a62bebee285c9e6192e3c0dd2b7a
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-05 19:58:23 +1000

    Created StyledErrorAlert component

---
+++ client/src/components/styles/Alerts.styled.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledErrorAlert = styled.div`
+    width: 100%;
+    min-height: 45px;
+    background-color: #f88;
+    border-radius: 5px;
+    margin: 0 0 2em 0;
+    line-height: 45px;
+    padding: 6px 12px;
+    color: #fff;
+    font-size: 0.85rem;
+    animation: fade 0.75s linear;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    span {
+        cursor: pointer;
+    }
+
+    p {
+        max-width: 75%;
+        line-height: 2;
+    }
+`;
