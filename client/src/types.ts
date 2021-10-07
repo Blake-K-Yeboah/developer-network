1fe06723dc9e2e755c888201aff53e0a39f682ee
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-07 19:36:22 +1000

    Added Comments

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,3 +1,4 @@
+// User Interface
 export interface IUser {
     _id: string;
     profilePicture: string;
@@ -14,12 +15,14 @@ export interface IUser {
     ];
 }
 
+// Auth Slice Initial State Interface
 export interface IAuthSliceState {
     user: IUser | null;
     isAuthenticated: boolean;
     token: string | null;
 }
 
+// Theme Interface
 export interface ITheme {
     colors: {
         lightGray: string;
