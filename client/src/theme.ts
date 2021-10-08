a7c9b08b6520261f14e64c31353ae3022bb69444
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-08 21:35:01 +1000

    Added fadedGray color to theme

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -4,6 +4,7 @@ export const theme = {
         primaryBlue: "#D2E1FF",
         primaryRed: "#FFD2D2",
         fadedWhite: "rgba(255, 255, 255, 0.2)",
+        fadedGray: "rgba(150, 150, 150, 0.2)",
         primaryDarkerRed: "#FFB1B1",
         primaryDarkerBlue: "#B8D0FF",
     },
