de1a381e67df47481c5059ec97560275f0d65a20
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-10 18:35:30 +1000

    Added inputs and not functional buttons

--- client/src/components/pages/Sign Up/MultiStepSignUpForm.tsx
+++ client/src/components/pages/Sign Up/MultiStepSignUpForm.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Styled Components
 import {
+    StyledButton,
+    StyledButtonGroup,
+    StyledForm,
     StyledFormTitle,
+    StyledInput,
+    StyledInputLabel,
     StyledSingleFormContainer,
 } from "../../styles/FormContainer.styled";
 
@@ -15,11 +20,80 @@ interface IProps {
     setStep: (newStep: number) => void;
 }
 
-const MultiStepSignUpForm: React.FC<IProps> = ({ step }) => {
+// User Input Interface
+interface IUserInput {
+    name: string;
+    email: string;
+    username: string;
+    bio: string;
+    password: string;
+}
+
+const MultiStepSignUpForm: React.FC<IProps> = ({ step, setStep }) => {
+    const userInputFromLocal = localStorage.getItem("userInput")
+        ? // @ts-ignore
+          JSON.parse(localStorage.getItem("userInput"))
+        : {};
+
+    const [userInput, setUserInput] = useState<IUserInput>({
+        name: "",
+        email: "",
+        username: "",
+        bio: "",
+        password: "",
+    });
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserInput({ ...userInput, [e.target.id]: e.target.value });
+    };
+    useEffect(() => {
+        Object.keys(userInput).forEach((key) => {
+            if (key in userInputFromLocal) {
+                setUserInput({
+                    ...userInput,
+                    [key]: userInputFromLocal.key,
+                });
+            }
+        });
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <StyledSingleFormContainer>
             <ProgressBar step={step} />
             <StyledFormTitle>Sign Up</StyledFormTitle>
+            <StyledForm>
+                {step === 1 ? (
+                    <>
+                        <StyledInputLabel>What is your name?</StyledInputLabel>
+                        <StyledInput
+                            placeholder="Name: "
+                            value={userInput.name}
+                            id="name"
+                            onChange={onChange}
+                        />
+
+                        <StyledInputLabel>What is your email?</StyledInputLabel>
+                        <StyledInput
+                            placeholder="Email: "
+                            type="email"
+                            value={userInput.email}
+                            id="email"
+                            onChange={onChange}
+                        />
+                    </>
+                ) : (
+                    ""
+                )}
+            </StyledForm>
+            <StyledButtonGroup>
+                <StyledButton colorScheme="red" notCenter disabled={step === 1}>
+                    Go Back
+                </StyledButton>
+                <StyledButton colorScheme="blue" notCenter>
+                    Continue
+                </StyledButton>
+            </StyledButtonGroup>
         </StyledSingleFormContainer>
     );
 };
